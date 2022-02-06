f4238327fc9c9c3c7a6a3512042966e7a45bf5c0
Joana Martins <joana.mafalda.97@hotmail.com>
2022-02-06 22:24:37 +0000

    New main with login bit

--- src/main.py
+++ src/main.py
@@ -1,39 +1,20 @@
+from crypt import methods
 from flask import Flask, redirect, url_for, render_template
 
 
-# Initiates the site
 app = Flask(__name__)
-
-
-# derterminar o que aparece na "home" => "/" 
-# Se content não estiver definido não aparece de todo
 
 @app.route("/")
 def home():
-    # in line html can be returned in a function 
-    return render_template("index.html", content="test", list=["Ju", "André", "Tsuki"])
+    return render_template("index.html")
 
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    pass
 
-# podem ser inseridas "variáveis" no argumento do "decorador", namely 
-# entre <> temos uma variável, enquanto sem <> tem de se escrever mesmo esse valor
-# Isto pode ser feito através do template, com {{}} no doc de html 
-
-@app.route("/<name>")
-def content(name):
-    return render_template("index.html", content=name, r=2, list=[name])
-
-# ou caso seja algo mais simples pode ser feito como está aqui, 
-# com uma f-string
-
-# @app.route("/<name>")
-# def user(name):
-#     return f"Hello {name}!"
-
-@app.route("/admin")
-def admin():
-    return redirect(url_for("user", name="Admin!"))
-
-# Initiates the site and debug True faz com que o 
-# programa detete as alterações no código e vá actualizando o site
 if __name__ == "__main__":
     app.run(debug=True)
+
+#   get is more general and is not private
+#   post is more secure and it won't be saved in web
+# server
